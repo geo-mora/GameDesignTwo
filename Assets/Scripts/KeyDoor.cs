cf1a5944acf17dafae32cbacd5702945af4828fb
geo-mora <geovannym474@gmail.com>
2024-10-31 13:08:33 -0400

    Added instructions
    Instructions + delete when player opens door

--- Assets/Scripts/KeyDoor.cs
+++ Assets/Scripts/KeyDoor.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class KeyDoor : MonoBehaviour
 {
+
+    public GameObject Instructions;
+
    void OnTriggerEnter(Collider collider) {
     if (collider.gameObject.name =="player" && GameVariables.keyCount>0) {
         GameVariables.keyCount--;
         Destroy(gameObject);
+        Instructions.SetActive(false);
     }
    }
 }
